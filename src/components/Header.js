5c22612e6f0c55a036278ef2f6adb012ae522979
rscheiwe <rscheiwe@gmail.com>
2018-09-11 22:47:24 -0400

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import '../Browse.css'
 // import { Card } from 'mdbreact';
 
 
 const Header = () => {
   return (
 
-      <div className="hoja">
-        <img src={require('./jacoto_logo.png')} alt="" width="200px"/>
+    <div style={{backgroundColor:'rgb(255,255,244)'}}>
+      <div className='head-div'>
+        <div className='blanket-div'>
+          <div className='flex-div'>
+            <div className='img-div'>
+                <img className='img-back-advanced' alt="" src={require('./jacoto-header.jpg')} />
+            </div>
+            <div className='txt-div'>
+              <h1>
+                <span className="advancedElement">
+                </span>
+                  <span className='open-div-advanced'>
+                    <img src={require('./jacoto_logo.png')} alt="" width="300px"/>
+                  </span>
+              </h1>
+              <p className='mirror-div'><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque vitae enim id aliquet. Quisque nec lorem pretium, posuere felis a, iaculis ex. Nulla tincidunt a nibh eget tempor.</b>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+
+      //       <div className="hoja">
+      //
+      // </div>
 
 
 
