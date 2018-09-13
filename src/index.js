d060181d6db588e2542cd72099e5bdc4e98a3103
rscheiwe <rscheiwe@gmail.com>
2018-09-12 20:40:33 -0400

    removed devtools

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import thunk from 'redux-thunk'
 
 let store = createStore(rootReducer,
   compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    )
   )
 
 ReactDOM.render((
