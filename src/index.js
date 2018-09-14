08552cfc00a5ff1cc02282c86d06c087534a1fa1
rscheiwe <rscheiwe@gmail.com>
2018-09-14 07:23:45 -0400

    final push till next refactoring

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import thunk from 'redux-thunk'
 
 let store = createStore(rootReducer,
   compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
   )
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render((
   <Provider store={store} >
