d3a946cbcf02a5e59a03eea9db5a3c7b9923949f
rscheiwe <rscheiwe@gmail.com>
2018-09-14 07:25:38 -0400

    final push till next refactoring

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ import registerServiceWorker from './registerServiceWorker';
 import thunk from 'redux-thunk'
 
 let store = createStore(rootReducer,
-  compose(applyMiddleware(thunk),
+  compose(applyMiddleware(thunk)
   )
+)
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render((
